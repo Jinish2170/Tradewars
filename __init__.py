9331930fb72effaf32252836a4c2fad191d03b04
Jinish Kathiriya <jinishkathiriya@gmail.com>
2025-02-23 20:43:29 +0530

    c

--- __init__.py
+++
@@ -1,8 +0,0 @@
-# This makes the root directory a proper Python package
-import sys
-from pathlib import Path
-
-# Add the project root to Python path
-ROOT_DIR = str(Path(__file__).parent)
-if ROOT_DIR not in sys.path:
-    sys.path.insert(0, ROOT_DIR)
--- simulation/__init__.py
+++
@@ -1,6 +0,0 @@
-# Make simulation a proper package
-
-from .market_simulation import market_session
-from .market_state import *
-
-__all__ = ['market_session']
